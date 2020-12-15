22d1526cb518cbe42fa88ede7e5549a92a8a0860
zPirroZ3007 <giusepirro@hotmail.com>
2020-12-15 03:21:02 +0100

    Fix possibile falla nelle lobby

--- src/main/java/it/founderhunt/lobby/listeners/AuthMeListener.java
+++ src/main/java/it/founderhunt/lobby/listeners/AuthMeListener.java
@@ -1,7 +1,10 @@
 package it.founderhunt.lobby.listeners;
 
+import com.connorlinfoot.titleapi.TitleAPI;
 import fr.xephi.authme.events.LoginEvent;
+import it.founderhunt.lobby.util.Icons;
 import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -10,7 +13,14 @@ public class AuthMeListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onLogin(LoginEvent event) {
-        event.getPlayer().setGameMode(GameMode.CREATIVE);
+        Player player = event.getPlayer();
+
+        player.setGameMode(GameMode.CREATIVE);
+        TitleAPI.sendTitle(player, 3, 60, 3, "§c§lFounderHunt - Benvenuto!", "§7Seleziona una partita per iniziare");
+
+        player.getInventory().clear();
+        player.getInventory().setItem(4, Icons.SELECTOR);
+        player.getInventory().setHeldItemSlot(4);
     }
 
     public static Listener to() {
--- src/main/java/it/founderhunt/lobby/listeners/JoinQuitListener.java
+++ src/main/java/it/founderhunt/lobby/listeners/JoinQuitListener.java
@@ -1,18 +1,13 @@
 package it.founderhunt.lobby.listeners;
 
-import com.connorlinfoot.titleapi.TitleAPI;
 import it.founderhunt.lobby.objects.LobbyPlayer;
-import it.founderhunt.lobby.util.Icons;
 import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
 public class JoinQuitListener implements Listener {
 
@@ -28,15 +23,6 @@ public class JoinQuitListener implements Listener {
             p.hidePlayer(event.getPlayer());
             event.getPlayer().hidePlayer(p);
         }
-
-        player.addPotionEffects(new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, 1, false, false));
-        player.setGameMode(GameMode.CREATIVE);
-
-        TitleAPI.sendTitle(player, 3, 60, 3, "§c§lFounderHunt - Benvenuto!", "§7Seleziona una partita per iniziare");
-
-        player.getInventory().clear();
-        player.getInventory().setItem(4, Icons.SELECTOR);
-        player.getInventory().setHeldItemSlot(4);
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
